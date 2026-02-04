f88201d975e23377691cd94afdfd934421baf6ec
Mohammad Osama <ansariosama024@gmail.com>
2026-02-05 00:51:24 +0530

    Add files via upload

---
+++ even.py
@@ -0,0 +1,9 @@
+num=int(input("Enter a number:"))
+if num>0:
+    print("Positive number")
+    if num%2==0:
+        print(" its Even")
+    else:
+        print(" Its Odd")
+else:
+    print("Not a positive number")    
